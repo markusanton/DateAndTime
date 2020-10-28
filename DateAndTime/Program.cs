81abee4c4f0f0fa182eb741b51526564927c50ca
markusanton <markusa070@gmail.com>
2020-10-28 15:49:42 +0200

    Add project files.

---
+++ DateAndTime/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace DateAndTime
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime today = DateTime.Today;
+            Console.WriteLine("2day: " + today);
+            
+            int yearToday = today.Year;
+            Console.WriteLine("current year: " + yearToday);
+
+            var dayOfWeekNow = today.DayOfWeek;
+            Console.WriteLine("day: " + dayOfWeekNow);
+
+            DateTime todayNow = DateTime.Now;
+            Console.WriteLine("local time: " + todayNow);
+            DateTime UTC = DateTime.UtcNow;
+            Console.WriteLine("utc: " + UTC);
+
+            DateTime xmasEve = new DateTime(yearToday, 12, 24);
+            var xmasEveWeek = xmasEve.DayOfWeek;
+            Console.WriteLine(xmasEveWeek);
+        }
+    }
+}
